ea7b070dc68009c6d24a37b4538153d505f964cf
punnyapradeep11 <49022622+punnyapradeep11@users.noreply.github.com>
2019-04-09 18:47:03 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var serverless = require('serverless-http'); //wraps apis in serverless
-var app = express();
-var routers = require('./routers/router')
-app.use(routers);
-
-module.exports.handler = serverless(app);
